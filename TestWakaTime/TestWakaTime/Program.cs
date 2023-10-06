8b1aadd14b54a5e3fb78ace06dc7eb6503450972
As1ss <alexis.lopbri@educa.jcyl.es>
2023-10-06 17:00:17 +0200

    Test from WakaTime successfull

---
+++ TestWakaTime/TestWakaTime/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+String nombre = "Campeón de las Telas";
+Console.WriteLine("Hello, "+nombre+"!");
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
