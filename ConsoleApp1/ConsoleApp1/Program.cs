44da0c1a11ed804ecd66db511f8b03e4e0981c49
As1ss <alexis.lopbri@educa.jcyl.es>
2023-10-09 20:16:28 +0200

    Practica1 añadida

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cómo te llamas?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+
+
+            //Comentario de una línea
+       
+            Console.WriteLine($"\nHola {name} son las {currentDate:t} del día {currentDate:d}");
+            /*
+             * Comentario de vairas líneas
+             *
+             */
+
+
+        }
+    }
+}
---
+++ Practica1_Alexis-Lopez-Briongos/Practica1_Alexis-Lopez-Briongos/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Practica1_Alexis_Lopez_Briongos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola buenas, introduce tu nombre.");
+            var name = Console.ReadLine();
+
+            menuPrincipal();
+        }
+
+        private static void menuPrincipal()
+        {
+
+            Console.WriteLine("Introduce la opción deseada:" +
+                "\n1.- Obtener la letra del dni" +
+                "\n2.- Obtener el día de la semana " +
+                "\n3.- Salir.");
+            try
+            {
+                
+                switch (opcion)
+                {
+                    case 1:
+
+                        break;
+                }
+            }
+            catch
+            {
+
+            }
+           
+        }
+    }
+}
