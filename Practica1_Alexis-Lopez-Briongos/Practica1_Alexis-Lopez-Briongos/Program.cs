ad94e4f2a600323280976f49022fa84c89435752
As1ss <alexis.lopbri@educa.jcyl.es>
2023-10-10 19:52:47 +0200

    Practica 1 realizada y entegada

--- Practica1_Alexis-Lopez-Briongos/Practica1_Alexis-Lopez-Briongos/Program.cs
+++ Practica1_Alexis-Lopez-Briongos/Practica1_Alexis-Lopez-Briongos/Program.cs
@@ -1,43 +1,50 @@
-﻿namespace Practica1_Alexis_Lopez_Briongos
+﻿using System.Globalization;
+
+//Práctica 1 en consola. Aplicación de obtención de dni y fecha realizada por Alexis López Briongos Dam2t.
+
+namespace Practica1_Alexis_Lopez_Briongos
 {
     internal class Program
     {
         private static int opcion;
         private static Boolean exit;
+        private static String input;
+        private static String name;
         static void Main(string[] args)
         {
             exit = false;
-            Console.WriteLine("Hola buenas, introduce tu nombre.");
-            var name = Console.ReadLine();
+            Console.WriteLine("Buenas tardes, introduce tu nombre.");
+            name = Console.ReadLine();
+          
 
             menuPrincipal();
         }
 
         private static void menuPrincipal()
         {
+            
             do
             {
 
 
-                Console.WriteLine("Introduce la opción deseada:" +
+                Console.WriteLine("\nIntroduce la opción deseada:" +
                     "\n1.- Obtener la letra del dni" +
                     "\n2.- Obtener el día de la semana " +
                     "\n3.- Salir.");
-                Console.WriteLine(opcion);
+                input = Console.ReadLine();
+                
 
-                if (int.TryParse(Console.ReadLine(),out opcion))
+                if (int.TryParse(input,out opcion))
                 {
+                    
                    
                     switch (opcion)
                     {
-                        case 0:
-                            Console.WriteLine("No has introducido ningún valor.");
-                            break;
                         case 1:
-                            Console.WriteLine("Has escogido la opcion 1.");
+                            getDniLetterOption();
                             break;
                         case 2:
-                            Console.WriteLine("Has escogido la opción 2.");
+                            getDateOption();
                             break;
                         case 3:
                             exit = true;
@@ -57,7 +64,43 @@
               
             } while (exit==false);
 
+            Console.WriteLine($"Gracias por su visita {name}");
            
+        }
+
+        private static void getDateOption()
+        {
+            Console.Clear();
+            DateTime date = DateTime.Now;
+            CultureInfo spanishCulture = new CultureInfo("es-ES");
+            Console.WriteLine($"Hoy es {date.ToString("dddd",spanishCulture)}");
+         
+        }
+
+        private static void getDniLetterOption()
+        {
+            Console.Clear();
+            Console.WriteLine("Obtener letra del dni.");
+            Console.WriteLine("\nIntroduce el número de tu dni");
+            string inputNum = Console.ReadLine();
+            int dniNum;
+            int.TryParse(inputNum, out dniNum);
+            obtainDNI(dniNum);
+        }
+
+        private static void obtainDNI(int dni)
+        {
+            char[] dniLettersValue = { 'T', 'R', 'W', 'A', 'G', 'M','Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+           
+            int resto;
+
+            resto = dni % 23;
+
+
+            Console.WriteLine($"Tu dni es {dni}-{dniLettersValue[resto]}");
+          
+
+
         }
     }
 }
