8c67d21f662a3b6222ef927a421c994f6d25a258
As1ss <alexis.lopbri@educa.jcyl.es>
2023-10-17 01:26:21 +0200

    Interfaz realizada

--- Practica2_Alexis-Lopez-Briongos/MainWindow.xaml.cs
+++ Practica2_Alexis-Lopez-Briongos/MainWindow.xaml.cs
@@ -28,42 +28,76 @@ namespace Practica2_Alexis_Lopez_Briongos
         private void textNombre_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             textNombre.Text = "";
+            textNombre.Foreground = new SolidColorBrush(Colors.Black);
         }
         private void textNombre_MouseLeave(object sender, MouseEventArgs e)
         {
-            textNombre.Text = "Ej: Roberto Joao";
+            if (textNombre.Text.Equals(""))
+            {
+                textNombre.Foreground = new SolidColorBrush(Colors.Gray);
+                textNombre.Text = "Ej: Roberto Joao";
+            }
+
         }
 
         private void textApellido_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             textApellido.Text = "";
+            textApellido.Foreground = new SolidColorBrush(Colors.Black);
         }
 
         private void textApellido_MouseLeave(object sender, MouseEventArgs e)
         {
-            textApellido.Text = "Ej: Tirón Gonsales";
+
+            if (textApellido.Text.Equals(""))
+            {
+                textApellido.Foreground = new SolidColorBrush(Colors.Gray);
+                textApellido.Text = "Ej: Tirón Gonsales";
+            }
+
         }
 
         private void textDNI_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             textDNI.Text = "";
+            textDNI.Foreground = new SolidColorBrush(Colors.Black);
         }
 
         private void textDNI_MouseLeave(object sender, MouseEventArgs e)
         {
-            textDNI.Text = "Ej: 12345678N";
+            if (textDNI.Text.Equals(""))
+            {
+                textDNI.Foreground = new SolidColorBrush(Colors.Gray);
+                textDNI.Text = "Ej: 12345678N";
+            }
+
         }
 
         private void textEmail_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             textEmail.Text = "";
+            textEmail.Foreground = new SolidColorBrush(Colors.Black);
         }
 
         private void textEmail_MouseLeave(object sender, MouseEventArgs e)
         {
-            textEmail.Text = "Ej: JohnDoe@gmail.com";
+            if (textEmail.Text.Equals(""))
+            {
+                textEmail.Foreground = new SolidColorBrush(Colors.Gray);
+                textEmail.Text = "Ej: JohnDoe@gmail.com";
+            }
 
 
+
+
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            textNombre.Text = "";
+            textApellido.Text = "";
+            textDNI.Text = "";
+            textEmail.Text = "";
         }
     }
 }
