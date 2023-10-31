7314b0dbd0a53e2d886e8f3a6907ef14005eeae5
As1ss <alexis.lopbri@educa.jcyl.es>
2023-11-01 00:45:03 +0100

    Proyecto Final: añadido dialog settings para configurar dificultades y eventos de salir y cerrar la ventana

--- Proyecto_1EVA_ALB/MainWindow.xaml.cs
+++ Proyecto_1EVA_ALB/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace Proyecto_1EVA_ALB
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool modoFacil;
+        public bool modoMedio;
+        public bool modoDios;
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void btnJugar_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+            MessageBox.Show("Modo facil: " + modoFacil + "\nModo medio: " + modoMedio + "\nModo Dios: " + modoDios);
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            // Crear e instanciar el diálogo personalizado
+            var dialog = new Settings(this);
+
+            // Mostrar el diálogo como una ventana emergente
+            dialog.ShowDialog();
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+      
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Muestra un mensaje de confirmación
+            MessageBoxResult result = MessageBox.Show("¿Seguro que deseas cerrar el juego?", "Confirmación", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.OK)
+            {
+                try
+                {
+                    this.Close();
+                }
+                catch
+                {
+                    Console.WriteLine("Ha ocurrido algún tipo de error");
+                }
+              
+            }
+            else
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
---
+++ Proyecto_1EVA_ALB/Settings.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_1EVA_ALB
+{
+    /// <summary>
+    /// Lógica de interacción para Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        private MainWindow mainWindow;
+      
+        public Settings(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+       
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            if (opcFacil.IsSelected)
+            {
+                mainWindow.modoFacil = true;
+                mainWindow.modoMedio = false;
+                mainWindow.modoDios = false;
+            }
+            if (opcMedio.IsSelected) {
+                mainWindow.modoFacil = false;
+                mainWindow.modoMedio = true;
+                mainWindow.modoDios = false;
+            }
+            if (opcModoDios.IsSelected)
+            {
+                mainWindow.modoFacil = false;
+                mainWindow.modoMedio = false;
+                mainWindow.modoDios = true;
+            }
+
+            this.Close();
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
