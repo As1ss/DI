3dd0dd62762276225107c61c37d837ed2d840cda
As1ss <alexis.lopbri@educa.jcyl.es>
2023-11-13 18:26:52 +0100

    .

--- Proyecto_1EVA_ALB/obj/Release/net7.0-windows/Nivel3.g.i.cs
+++ Proyecto_1EVA_ALB/obj/Release/net7.0-windows/Nivel3.g.i.cs
@@ -157,7 +157,7 @@ namespace Proyecto_1EVA_ALB {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Proyecto_1EVA_ALB;V1.0.0.0;component/nivel3.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Proyecto_1EVA_ALB;component/nivel3.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Nivel3.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
