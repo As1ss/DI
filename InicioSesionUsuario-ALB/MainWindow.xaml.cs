84b2e2e1c9822849440b88517c6bd5c063b562bb
As1ss <alexis.lopbri@educa.jcyl.es>
2023-12-09 23:54:14 +0100

    Practica login desarrollado interfaz basica y prueba de conexion y obtencion de datos

---
+++ InicioSesionUsuario-ALB/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using MySqlConnector;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InicioSesionUsuario_ALB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const String SERVIDOR= "localhost";
+        private const uint PUERTO = 3306;
+        private const String BASEDATOS = "usuariologin";
+        private const String USUARIO = "root";
+        private const String PASSWORD = "1234";
+        private String? nombreUsuario;
+        private String? contrasenaUsuario;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            
+             nombreUsuario = tbxUsuario.Text;
+             contrasenaUsuario = bxPassword.Password;
+
+
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
+            {
+                Server = SERVIDOR,
+                Port = PUERTO,
+                Database = BASEDATOS,
+                UserID = USUARIO,
+                Password = PASSWORD
+            };
+
+            using var connector = new MySqlConnection(builder.ToString());
+
+            try
+            {
+                connector.Open();
+
+                String consulta = "SELECT nombre,contrasena FROM usuario";
+
+                using var command = new MySqlCommand(consulta, connector);
+
+                using var reader = command.ExecuteReader();
+                
+                String userBDD = "";
+                String passwordBDD = "";
+
+
+                  while(reader.Read()) { 
+
+                    userBDD = reader.GetString(0);
+                    passwordBDD= reader.GetString(1);
+
+                   
+
+                }
+
+                if (nombreUsuario.Equals(userBDD) && contrasenaUsuario.Equals(passwordBDD))
+                {
+                    
+                    MessageBox.Show("LOGIN CORRECTO");
+
+                }
+                else
+                {
+                    MessageBox.Show("LOGIN INCORRECTO");
+                }
+
+
+
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+
+
+            
+
+        }
+    }
+}
