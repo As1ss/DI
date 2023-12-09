17f82d174db326ea91a51ea4ea01143e79ce2cdf
As1ss <alexis.lopbri@educa.jcyl.es>
2023-12-09 19:07:20 +0100

    Ejemplo conexion base de datos

---
+++ TestConexionBDD/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using MySqlConnector;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestConexionBDD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String servidor = tbxServidor.Text;
+            uint puerto = uint.Parse(tbxPuerto.Text);
+            String bDD = tbxBDD.Text;
+            String usuario = tbxUsuario.Text;
+            String password = bxPassword.Password;
+
+
+
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
+            {
+                Server = servidor,
+                Port=puerto,
+                Database = bDD,
+                UserID = usuario,
+                Password = password
+
+            };
+
+            using var connectBDD = new MySqlConnection(builder.ToString());
+
+            try
+            {
+                connectBDD.Open();
+                String consultaBDD = "SHOW DATABASES;";
+                using var comando = new MySqlCommand(consultaBDD,connectBDD);
+                using var reader = comando.ExecuteReader();
+                String data = "";
+
+                while(reader.Read())
+                {
+                    data +=$"\n{reader.GetString(0)}";
+                }
+
+                MessageBox.Show($"Bases de datos: {data}");
+
+
+
+            }catch(MySqlException ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+            finally
+            {
+                connectBDD.Close();
+            }
+
+
+
+        }
+
+    }
+}
