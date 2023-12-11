bad48ff40137305b557325f243a7576cd5590c73
As1ss <alexis.lopbri@educa.jcyl.es>
2023-12-11 01:43:31 +0100

    Practica login realizada logica de setear y getear fallos de inicio de sesion

--- InicioSesionUsuario-ALB/MainWindow.xaml.cs
+++ InicioSesionUsuario-ALB/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MySqlConnector;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,83 +18,106 @@ namespace InicioSesionUsuario_ALB
     /// </summary>
     public partial class MainWindow : Window
     {
-       
+
         private String? nombreUsuario;
         private String? contrasenaUsuario;
 
         public MainWindow()
         {
             InitializeComponent();
-          
+
         }
 
         private void btnIniciar_Click(object sender, RoutedEventArgs e)
         {
-            
-             nombreUsuario = tbxUsuario.Text;
-             contrasenaUsuario = bxPassword.Password;
+
+            nombreUsuario = tbxUsuario.Text;
+            contrasenaUsuario = bxPassword.Password;
 
 
-  
+
 
             try
             {
-               MySqlConnection connector = ConexionBD.getConnection();
+                MySqlConnection connector = ConexionBD.getConnection();
 
-                String consulta = "SELECT nombre,contrasena FROM usuario";
+                String consulta = $"SELECT nombre,contrasena FROM usuario WHERE nombre = '{nombreUsuario}'";
 
                 using var command = new MySqlCommand(consulta, connector);
 
                 using var reader = command.ExecuteReader();
-                
+
                 String userBDD = "";
                 String passwordBDD = "";
 
 
-                  while(reader.Read()) { 
+                while (reader.Read())
+                {
 
                     userBDD = reader.GetString(0);
-                    passwordBDD= reader.GetString(1);
+                    passwordBDD = reader.GetString(1);
 
-                   
+
 
                 }
 
+                reader.Close();
+
                 if (nombreUsuario.Equals(userBDD) && contrasenaUsuario.Equals(passwordBDD))
                 {
-                    
-                  
-                   
-                    connector.Close(); 
-                    connector.Open();
 
-                    using (MySqlCommand commando = new MySqlCommand("SELECT tipo_usuario FROM usuario",connector))
+                    using (MySqlCommand commando = new MySqlCommand($"SELECT * FROM usuario WHERE usuario.nombre = '{userBDD}'", connector))
                     {
-                        commando.ExecuteReader(); 
+                        commando.ExecuteReader();
                         if (reader.Read())
                         {
-                            MessageBox.Show($"LOGIN CORRECTO: \nNombre: {userBDD} \nTipo de usuario: {reader.GetString(0)}");
+                            MessageBox.Show($"LOGIN CORRECTO: \nID: {reader.GetInt16(0)}\nNombre: {reader.GetString(1)}\nContraseña: {reader.GetString(2)}\nTipo de usuario: {reader.GetString(3)}\nIntentos fallidos: {reader.GetInt16(4)}\nBloqueado: {reader.GetBoolean(5)}");
+
+                            tbMensaje.Text = "Credenciales correctas";
                         }
                     }
-
+                    reader.Close() ;
+                }
+                else if(nombreUsuario.Equals(userBDD) && !contrasenaUsuario.Equals(passwordBDD))
+                {
+                    setFallos(userBDD, reader,connector);
+                    tbMensaje.Text=$"Tienes {getFallos(userBDD,reader,connector)} fallos.";
                 }
                 else
                 {
                     MessageBox.Show("LOGIN INCORRECTO");
+                    tbMensaje.Text = "El usuario no se encuentra en nuestra base de datos";
                 }
-
-
-
-
             }
             catch (MySqlException ex)
             {
                 MessageBox.Show($"Error: {ex.Message}");
             }
+        }
+        
+        private void setFallos(String usuarioNombre,MySqlDataReader reader,MySqlConnection connector)
+        {
+            tbMensaje.Text = "Contraseña incorrecta.";
+            using (MySqlCommand aumentoFallos = new MySqlCommand($"UPDATE usuario SET intentos_fallidos=intentos_fallidos+1 WHERE nombre = '{usuarioNombre}'", connector))
+            {
+                aumentoFallos.ExecuteReader();
+            }
+            reader.Close();
+        }
+        private int getFallos(String usuarioNombre,MySqlDataReader reader, MySqlConnection connector)
+        {
+            int numFallos=0;
 
-
-            
-
+            using(MySqlCommand obtenerFallos = new MySqlCommand($"SELECT intentos_fallidos FROM usuario WHERE nombre = '{usuarioNombre}'", connector))
+            {
+                obtenerFallos.ExecuteReader();
+                if(reader.Read())
+                {
+                    numFallos = reader.GetInt16(0);
+                }
+               
+            }
+           return numFallos;
         }
     }
 }
