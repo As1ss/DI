b4cfdac2215e38c30cb69858fd7aa81cf1cda00f
As1ss <alexis.lopbri@educa.jcyl.es>
2023-12-12 21:37:28 +0100

    Merge branch 'main' of https://github.com/As1ss/DI

---
+++ TestBindings/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestBindings
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Persona p;
+        public MainWindow()
+        {
+            InitializeComponent();
+            p= new Persona();
+            p.Nombre = "Alexis";
+            p.Apellido = "López";
+            p.Edad = 21;
+            p.NombreCompleto = "";
+
+            this.DataContext = p;
+            
+        }
+
+        private void btnMostrar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{p.Nombre} tiene la edad de {p.Edad}");
+        }
+    }
+}
---
+++ TestBindings/Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBindings
+{
+    public class Persona:INotifyPropertyChanged
+    {
+        private String? nombre;
+        private String? apellido;
+        private String? nombreCompleto;
+        private uint edad;
+        
+
+        public Persona() { 
+
+        }
+
+        public string? Nombre { get => nombre; set
+            {
+                nombre = value;
+                OnPropertyChanged("NombreCompleto");
+            }
+        }
+        public uint Edad { get => edad; set => edad = value; }
+        public string? Apellido
+        {
+            get => apellido; set
+            {
+                apellido = value;
+                OnPropertyChanged("ApellidoCompleto");
+            }
+        }
+        public string? NombreCompleto
+        {
+            get => $"{Nombre} {Apellido}";
+            set { }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
