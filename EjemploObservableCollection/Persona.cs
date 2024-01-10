e57e956d700f8145de930631a3c9655c5361d253
As1ss <alexis.lopbri@educa.jcyl.es>
2024-01-10 20:02:51 +0100

    Proyecto 2 eva creado

---
+++ EjemploObservableCollection/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploObservableCollection
+{
+
+    public class Persona
+    {
+        private String nombre;
+        private DateTime fecha = DateTime.Now;
+        private int edad;
+
+        public Persona(string nombre, string apellido, int edad)
+        {
+            this.nombre = nombre;
+         
+            this.edad = edad;
+        }
+       
+
+        public String getNombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public String getApellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+        public int getEdad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+
+    }
+}
--- Practica2_Alexis-Lopez-Briongos/obj/Debug/net7.0-windows/VentanaResultado.g.i.cs
+++ Practica2_Alexis-Lopez-Briongos/obj/Debug/net7.0-windows/VentanaResultado.g.i.cs
@@ -95,13 +95,13 @@ namespace Practica2_Alexis_Lopez_Briongos {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Practica2_Alexis-Lopez-Briongos;component/ventanaresultado.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Practica2_Alexis-Lopez-Briongos;V1.0.0.0;component/ventanaresultado.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\VentanaResultado.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -111,7 +111,7 @@ namespace Practica2_Alexis_Lopez_Briongos {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
