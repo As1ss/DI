b8c2a41e9c92b033b0fb1bbe1854af8c91f87b36
As1ss <alexis.lopbri@educa.jcyl.es>
2024-02-02 15:09:31 +0100

    Proyecto 2 EVa fix nombre de las ventans y añadido del icono de la aplciacion

--- PROYECTO-EV2-ALB/obj/Debug/net8.0-windows/PROYECTO-EV2-ALB.AssemblyInfo.cs
+++ PROYECTO-EV2-ALB/obj/Debug/net8.0-windows/PROYECTO-EV2-ALB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROYECTO-EV2-ALB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24c2d8932715dbd36714866464fa954668946826")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROYECTO-EV2-ALB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROYECTO-EV2-ALB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
